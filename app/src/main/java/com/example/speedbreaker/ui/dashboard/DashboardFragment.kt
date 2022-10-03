e337bba03db2836dc83be004edaee0cd63aa5318
rumaan <rummaanumme@gmail.com>
2022-10-03 12:49:46 +0530

    displaying data

--- app/src/main/java/com/example/speedbreaker/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/speedbreaker/ui/dashboard/DashboardFragment.kt
@@ -1,12 +1,18 @@
 package com.example.speedbreaker.ui.dashboard
 
+import android.Manifest
+import android.app.ActivityManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.Sensor.*
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -16,11 +22,17 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.speedbreaker.*
 import com.example.speedbreaker.databinding.FragmentDashboardBinding
+import com.google.android.gms.location.LocationResult
 
 class DashboardFragment : Fragment(), View.OnClickListener, SensorEventListener {
 
@@ -33,21 +45,30 @@ class DashboardFragment : Fragment(), View.OnClickListener, SensorEventListener 
     lateinit var  sensorManager :SensorManager
     lateinit var sensorEvent: SensorEvent
 
-   lateinit var buttonPothole: Button
+    lateinit var latLngTextView:TextView
+    lateinit var speedTv: TextView
+
+    lateinit var buttonPothole: Button
+    lateinit var buttonHump: Button
+
    lateinit var xval: TextView
    lateinit var yval :TextView
    lateinit var zval :TextView
+
    lateinit var xGyroValue :TextView
    lateinit var yGyroValue :TextView
    lateinit var zGyroValue :TextView
+
    lateinit var xMagnoValue :TextView
    lateinit var yMagnoValue :TextView
    lateinit var zMagnoValue :TextView
-   lateinit var mGyro :Sensor
+
+
    lateinit  var  accelerometer: Sensor
+   lateinit var  btnStartLoc :Button
+   lateinit var  btnStopLoc :Button
 
 
-    lateinit var buttonHump: Button
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -94,32 +115,31 @@ class DashboardFragment : Fragment(), View.OnClickListener, SensorEventListener 
 
        // val textView: TextView = binding.textDashboard
         buttonHump = binding.btnHump
-        xval= binding.xValue
-        yval = binding.yValue
-        zval = binding.zValue
-        xGyroValue = binding.xGyroValue
-        yGyroValue = binding.yGyroValue
-        zGyroValue = binding.zGyroValue
-        xMagnoValue = binding.xMagnoValue
-        yMagnoValue = binding.yMagnoValue
-        zMagnoValue = binding.zMagnoValue
-
-
-
         buttonPothole = binding.btnPothole
         buttonHump.setOnClickListener(this)
         buttonPothole.setOnClickListener(this)
 
-      /*  yval.setText(accelerometer)
-        zval.setText(accelerometer)
-        xGyroValue.setText(mGyro)
-        yGyroValue.setText(mGyro)
-        zGyroValue.setText(mGyro)
+        xval= binding.xValue
+        yval = binding.yValue
+        zval = binding.zValue
 
-        xMagnoValue.setText(TYPE_MAGNETIC_FIELD)
-        yMagnoValue.setText(TYPE_MAGNETIC_FIELD)
-        zMagnoValue.setText(TYPE_MAGNETIC_FIELD)*/
+        xGyroValue = binding.xGyroValue
+        yGyroValue = binding.yGyroValue
+        zGyroValue = binding.zGyroValue
 
+        xMagnoValue = binding.xMagnoValue
+        yMagnoValue = binding.yMagnoValue
+        zMagnoValue = binding.zMagnoValue
+
+        btnStartLoc= binding.buttonStartLocationUpdates
+        btnStopLoc = binding.buttonStopLocationUpdates
+        btnStopLoc.setOnClickListener(this)
+        btnStartLoc.setOnClickListener(this)
+
+
+        latLngTextView = binding.tvLatlong
+        speedTv = binding.tvSpeed
+        
 
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             //textView.text = it
@@ -127,10 +147,12 @@ class DashboardFragment : Fragment(), View.OnClickListener, SensorEventListener 
         return root
     }
 
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 
     override fun onClick(viewClicked: View?) {
 
@@ -155,13 +177,123 @@ class DashboardFragment : Fragment(), View.OnClickListener, SensorEventListener 
 
             R.id.btnPothole -> {Log.i(TAG, "onClick: pothole detected")}
 
+            R.id.buttonStartLocationUpdates -> {
+                if (context?.applicationContext?.let {
+                        ContextCompat.checkSelfPermission(
+                            it, Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    } != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        DashboardFragment.REQUEST_CODE_LOCATION_PERMISSION
+                    )
+                } else {
+                    startLocationService()
+                }
+            }
+            R.id.buttonStopLocationUpdates -> { stopLocationService() }
+
+        }
+
+
+    }
+    override fun onStart() {
+        super.onStart()
+        LocalBroadcastManager.getInstance(requireContext())
+            .registerReceiver(mReceiver, IntentFilter("location.action"))
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == DashboardFragment.REQUEST_CODE_LOCATION_PERMISSION && grantResults.size > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationService()
+            } else {
+                Toast.makeText(activity, "Permission denied!", Toast.LENGTH_SHORT).show()
+                //Log.i(TAG,"Permission Denied")
+            }
         }
     }
 
+    private val isLocationServiceRunning: Boolean
+        private get() {
+            val activityManager = context?.getSystemService(AppCompatActivity.ACTIVITY_SERVICE) as ActivityManager
+            if (activityManager != null) {
+                for (service in activityManager.getRunningServices(Int.MAX_VALUE)) {
+                    if (LocationService::class.java.getName() == service.service.className) {
+                        if (service.foreground) {
+                            return true
+                        }
+                    }
+                }
+                return true
+            }
+            return false
+        }
+    private fun stopLocationService() {
+        if (isLocationServiceRunning) {
+            val intent = Intent(context?.applicationContext, LocationService::class.java)
+            intent.action = Constants.ACTION_STOP_LOCATION_SERVICE
+            context?.stopService(intent)
+            Toast.makeText(activity, "Location Service stopped", Toast.LENGTH_SHORT).show()
+            //Log.i(TAG,"Location Service Stopped")
+        }
+
+
+    }
+
+
+    private fun startLocationService() {
+        // if (isLocationServiceRunning) {
+        val intent = Intent(context?.applicationContext, LocationService::class.java)
+        intent.action = Constants.ACTION_START_LOCATION_SERVICE
+        context?.startService(intent)
+        //Log.i(TAG,"Location Service started")
+        Toast.makeText(activity, "Location Service started", Toast.LENGTH_SHORT).show()
+        //  }
+    }
+
+    private val mReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val updateno = intent.extras!!.getInt("updateno")
+            val locationResult: LocationResult? = intent.extras!!.getParcelable("location")
+            Log.d(
+                LocationActivity.TAG,
+                "Recieved message by BroadcastReciever: latitude =" + locationResult?.getLastLocation()?.getLatitude()
+            )
+            val location: Location = locationResult?.getLastLocation()!!
+            speedTv.setText(location.speed.toString()+"-m/s")
+            latLngTextView.text = """latitude =${location.latitude}
+                longitude =${location.longitude}
+                --$updateno
+                accuaracy${location.accuracy}
+                speed${location.speed}
+                """.trimIndent()
+
+
+
+        }
+    }
+
+
     companion object{
+        private const val REQUEST_CODE_LOCATION_PERMISSION = 1
+
         var TAG = DashboardFragment::class.java.simpleName
     }
 
+
+
+
+
+
+    
+    
     override fun onSensorChanged(senseEvent: SensorEvent?) {
         sensorEvent = senseEvent!!
          sensor = senseEvent!!.sensor
